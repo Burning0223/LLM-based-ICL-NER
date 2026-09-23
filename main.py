6cd8d991bb1ec2f3d900facfe349c02f42c5e2dd
Burning0223 <liuboning2004@126.com>
2026-09-23 13:40:49 +0800

    add resume logistion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 from concurrent.futures import ThreadPoolExecutor
+from json_repair import load
 from data_process import read_bc5cdr,build_prompt,save_json
 from utils import Argument,get_Labels,Metric
 from api import APIClient
@@ -14,6 +15,8 @@ class Eval():
         self.args=args
         self.metric=metric
         self.api_name=api_name
+        self.out_path=f"output/{self.api_name}_output.json"
+        self.failed_path=f"output/{self.api_name}_failed.json"
     def predict(self,example):     
         try:
             prompt=build_prompt(self.args.prompt,example["text"],examples=self.args.examples)
@@ -28,11 +31,37 @@ class Eval():
             }
         except Exception as e:
             print(f"请求失败：{e}")
-
-        return None
+            return None
     def eval(self):
-        with ThreadPoolExecutor(max_workers=10) as executor:
-            results=list(executor.map(self.predict,self.dataset))
+        try:
+            results=load(open(self.out_path, "rb"))
+            if len(results)!=len(self.dataset):
+                print("已有结果数量与数据集数量不一致，重新开始。")
+                results=[None]*len(self.dataset)
+            else:
+                 print("发现已有结果，继续执行。")
+        except FileNotFoundError:
+            print("没有发现已有结果，从头开始。")
+            results=[None]*len(self.dataset)
+        completed_count=0
+        for result in results:
+            if result is not None:
+                completed_count+=1
+        pending_idx=[
+            i
+            for i,result in enumerate(results)
+            if result is None
+        ]
+        print(f"已完成的请求：{completed_count}/{len(self.dataset)}")
+        print(f"未完成的请求：{len(pending_idx)}")
+        if pending_idx:
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                predictions=executor.map(self.predict,[self.dataset[i] for i in pending_idx])
+            for i,pred in zip(pending_idx,predictions):
+                results[i]=pred
+                save_json(results,self.out_path)
+        else:
+            print("所有数据都已经完成，不需要重新请求。")
         failed_results=[]
         for i,(pred,true) in enumerate(zip(results,self.dataset)):
             if pred is None:
@@ -45,6 +74,7 @@ class Eval():
         save_json(failed_results,f"output/{self.api_name}_failed.json")
         self.metric.report()
         print(f"请求总数：{len(self.dataset)}")
+        print(f"请求成功数量：{len(self.dataset)-len(failed_results)}")
         print(f"失败数量：{len(failed_results)}")
 
 def main(arg_path,api_name):
